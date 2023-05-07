8d177f90bf4ae4bfc0cdeb0c6d04ed61146ad544
Asset Kad <asset_kad@mail.ru>
2023-05-08 03:18:15 +0500

    finished stage 0, module #4.1, task#3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -1,6 +1,14 @@
 package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
-    public void vowelDeterminer(char character) {
+  public void vowelDeterminer(char character) {
+    char upperChar = Character.toUpperCase(character);
+    if ((upperChar < 'A') || (upperChar > 'Z')) {
+      System.out.println("wrong alphabet!");
+    } else if ("AEIOU".indexOf(upperChar) != -1) {
+      System.out.println("Vowel");
+    } else {
+      System.out.println("Consonant");
     }
+  }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -1,6 +1,17 @@
 package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
-    public void amountOfDays(int month) {
+  public void amountOfDays(int month) {
+    switch (month) {
+      case 6:
+        System.out.println(30);
+        break;
+      case 2:
+        System.out.println(28);
+        break;
+      default:
+        System.out.println("wrong number!");
+        break;
     }
+  }
 }
